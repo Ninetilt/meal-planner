ce160d54b8c024b52fc2a0fc95b9609ed76e178e
Ninetilt <schneeberger.julian@outllok.de>
2026-02-26 13:11:53 +0100

    initial setup

---
+++ meal-planner/build.gradle.kts
@@ -0,0 +1,28 @@
+val kotlin_version: String by project
+val logback_version: String by project
+
+plugins {
+    kotlin("jvm") version "2.3.0"
+    id("io.ktor.plugin") version "3.4.0"
+}
+
+group = "de.dhbw"
+version = "0.0.1"
+
+application {
+    mainClass = "de.dhbw.ApplicationKt"
+}
+
+kotlin {
+    jvmToolchain(21)
+}
+
+dependencies {
+    implementation("io.ktor:ktor-server-core")
+    implementation("io.ktor:ktor-server-default-headers")
+    implementation("io.ktor:ktor-server-call-logging")
+    implementation("io.ktor:ktor-server-netty")
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+    testImplementation("io.ktor:ktor-server-test-host")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+}
---
+++ meal-planner/settings.gradle.kts
@@ -0,0 +1,7 @@
+rootProject.name = "meal-planner"
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+    }
+}
