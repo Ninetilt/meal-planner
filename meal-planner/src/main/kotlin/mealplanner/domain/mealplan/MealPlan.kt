3045e3fb2af8a7d9532ed5b01aa30e4f56f50de1
Ninetilt <schneeberger.julian@outllok.de>
2026-03-23 19:03:28 +0100

    expanded meal plan domain
    name, members

--- meal-planner/src/main/kotlin/mealplanner/domain/mealplan/MealPlan.kt
+++ meal-planner/src/main/kotlin/mealplanner/domain/mealplan/MealPlan.kt
@@ -1,11 +1,44 @@
 package de.dhbw.mealplanner.domain.mealplan
 
+import de.dhbw.mealplanner.domain.user.UserId
 import java.util.UUID
 
 class MealPlan(
     val id: MealPlanId,
+    private var name: String,
+    val createdBy: UserId,
+    private val members: MutableSet<UserId> = mutableSetOf(),
     private val meals: MutableList<Meal> = mutableListOf()
 ) {
+
+    init {
+        require(name.isNotBlank()) { "MealPlan name must not be blank" }
+        members.add(createdBy)
+    }
+
+    fun changeName(newName: String) {
+        require(newName.isNotBlank()) { "MealPlan name must not be blank" }
+        name = newName
+    }
+
+    fun addMember(userId: UserId) {
+        members.add(userId)
+    }
+
+    fun removeMember(userId: UserId) {
+        require(userId != createdBy) { "Creator cannot be removed from meal plan" }
+
+        members.remove(userId)
+
+        meals.forEach { meal ->
+            meal.removeParticipant(userId)
+            meal.removeResponsible(userId)
+        }
+    }
+
+    fun getMembers(): Set<UserId> = members.toSet()
+
+    fun getName(): String = name
 
     fun createMeal(date: MealDate, type: MealType): Meal {
 
